0646aff0779f9962986997f18ad92744a0ec6907
francofresno <franco.fresno@gmail.com>
2019-11-18 22:30:28 -0300

    agrego frame a ventana y evito que sea rezisable

--- ventana.py
+++ ventana.py
@@ -18,6 +18,7 @@ class Ventana(ListaManager):
     def __init__(self,titulo):
         self.ventana.title(titulo)
         self.canvas = Canvas(self.ventana, height=600, width=600, bg="#263D42")
+        self.frame = Frame(self.ventana, bg="white")
         self.merge_sort = Button(self.ventana, text="Merge Sort", padx=10, pady=5, fg="white", bg="#263D42", command=partial(self.ordenar,Algoritmo(MergeSort())))
         self.quick_sort = Button(self.ventana, text="Quick Sort", padx=10, pady=5, fg="white", bg="#263D42", command=partial(self.ordenar,Algoritmo(QuickSort())))
         self.heap_sort = Button(self.ventana, text="Heap Sort", padx=10, pady=5, fg="white", bg="#263D42", command=partial(self.ordenar,Algoritmo(HeapSort())))
@@ -25,7 +26,9 @@ class Ventana(ListaManager):
         self.selec_archivo = Button(self.ventana, text="Seleccionar Archivo", padx=10, pady=5, fg="white", bg="#FF5733", command=self.archivo_a_lista)
         
     def configurar(self):
+        self.ventana.resizable(width=False, height=False)
         self.canvas.pack(fill=BOTH)
+        self.frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.08)
         self.merge_sort.pack(side=LEFT)
         self.quick_sort.pack(side=LEFT)
         self.heap_sort.pack(side=LEFT)
